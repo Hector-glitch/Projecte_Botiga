22909639b117dff766ff61ada9c39f2c5ef943ed
IanStyler <ian.soles@institutvidreres.cat>
2023-02-26 17:46:27 +0100

    Merge remote-tracking branch 'origin/PestanyaPerfils' into PestanyaPerfils
    # Conflicts:
    #	src/app/cataleg/cataleg.component.html
    #	src/app/cistella/cistella.component.html
    #	src/app/condicions/condicions.component.html
    #	src/app/contacte/contacte.component.html
    #	src/app/inici/inici.component.html
    #	src/app/inici/inici.component.ts
    #	src/app/login/login.component.ts
    #	src/app/perfil/perfil.component.html
    #	src/app/perfil/perfil.component.ts
    #	src/app/registre/registre.component.html

--- src/app/registre/registre.component.ts
+++ src/app/registre/registre.component.ts
@@ -24,7 +24,7 @@ export class RegistreComponent {
     this.nomAutenticat= 'null';
   }
   registrar(){
-
+    this.http.post<any>('http://localhost:3080/datausers',{Adreça:this.adreca,Cognoms:this.cognoms,Correu:this.correu,Nom:this.nom,Telèfon:this.telefon}).subscribe();
     this.http.post<any>('http://localhost:3080/signup', {email:this.correu, password: this.passwd}).subscribe();
     this.router.navigate(['/login'])
   }
