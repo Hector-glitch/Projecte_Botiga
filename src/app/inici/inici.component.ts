2d75cf414e9bdaae228739fe8c5c89961a3aca8a
Héctor <hector.vinas@institutvidreres.cat>
2023-05-22 09:48:08 +0200

    Info sobre llibres al inici

--- src/app/inici/inici.component.ts
+++ src/app/inici/inici.component.ts
@@ -1,5 +1,7 @@
-import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {UsuariService} from "../usuari.service";
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -7,18 +9,28 @@ import {UsuariService} from "../usuari.service";
   templateUrl: './inici.component.html',
   styleUrls: ['./inici.component.css','../../assets/css/Default.css']
 })
-export class IniciComponent {
+export class IniciComponent implements OnInit{
   nomAutenticat: any;
   autenticat = this.usuariServei.autenticat;
   root: any; //creem la boolean
+  book: any = {};
 
+  ngOnInit(): void {
+    this.http.get('https://www.googleapis.com/books/v1/volumes?q=random').subscribe((data: any) => {
+      const randomBook = data.items[Math.floor(Math.random() * data.items.length)];
+      this.book.title = randomBook.volumeInfo.title;
+      this.book.authors = randomBook.volumeInfo.authors;
+      this.book.description = randomBook.volumeInfo.description;
+      this.book.thumbnail = randomBook.volumeInfo.imageLinks.thumbnail;
+    });
+  }
   tancarSessio(){
     this.usuariServei.autenticat = false;
     this.autenticat= false;
     this.nomAutenticat= 'null';
     this.root=false;
   }
-  constructor(private renderer: Renderer2,private usuariServei: UsuariService) {
+  constructor(private renderer: Renderer2,private usuariServei: UsuariService, private http: HttpClient) {
     if(this.autenticat){
       this.nomAutenticat = this.usuariServei.arrClients.clients[this.usuariServei.posAutenticat].Nom;
 
